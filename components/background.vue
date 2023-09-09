6fc39f662e92e274f2c287922099aafec5279812
Michael Tischler <michael.tischler@towa.at>
2023-09-09 15:29:49 +0200

    feat: add all files of scene

---
+++ components/background.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="video-wrapper">
+  <Transition name="fade">
+    <video v-if="!startApp" src="~assets/video/appstart.mp4" autoplay muted loop>
+        <p>Your browser does not support the video tag.</p>
+    </video>
+  </Transition>
+    <video src="~assets/video/bgWaves.mp4" autoplay muted>
+        <p>Your browser does not support the video tag.</p>
+    </video>
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps({startApp: Boolean})
+
+
+</script>
+<style lang="scss" scoped>
+.video-wrapper {
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+
+  video {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s reverse;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ components/start-dream.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="video-wrapper-startdream">
+    <video ref="video">
+      <source src="~assets/video/gutenacht.mp4" type="video/mp4" />
+        <p>Your browser does not support the video tag.</p>
+    </video>
+    <Transition name="fadelong">
+      <div v-if="showOverlay" class="overlay" />
+    </Transition>
+  </div>
+</template>
+<script setup lang="ts">
+const video = ref(null);
+const showOverlay = ref(false);
+onMounted(() => {
+  if (video.value) {
+    video.value.play();
+  }
+
+
+  setTimeout(() => {
+    showOverlay.value = true;
+  }, 6000);
+
+});
+
+</script>
+<style lang="scss">
+.video-wrapper-startdream {
+  position: relative;
+  top: -5vh;
+  width: 100vw;
+  height: 100vh;
+  z-index: 20;
+
+  video {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    top: 0;
+    z-index: 15;
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    z-index: 16;
+  }
+}
+
+.fadelong-enter-active,
+.fadelong-leave-active {
+  transition: opacity 1s;
+}
+
+.fadelong-enter-from,
+.fadelong-leave-to {
+  opacity: 0;
+}
+</style>
